bedd62e50395932426cb274c05418b3546c7ebe2
Avestami <avestanabizadeh@gmail.com>
2025-05-28 23:39:43 +0330

    fixed uselang

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,17 +5,20 @@ import { SessionProvider } from 'next-auth/react';
 import { UserProvider } from './context/UserProvider';
 import { ErrorProvider } from './context/ErrorProvider';
 import { TerminalProvider } from './context/TerminalProvider';
+import { LanguageProvider } from './contexts/LanguageContext';
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <SessionProvider>
-      <UserProvider>
-        <ErrorProvider>
-          <TerminalProvider>
-            {children}
-          </TerminalProvider>
-        </ErrorProvider>
-      </UserProvider>
+      <LanguageProvider>
+        <UserProvider>
+          <ErrorProvider>
+            <TerminalProvider>
+              {children}
+            </TerminalProvider>
+          </ErrorProvider>
+        </UserProvider>
+      </LanguageProvider>
     </SessionProvider>
   );
 } 
