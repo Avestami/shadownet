ec9496212d52b8be76742a3704145e696513d949
Avestami <avestanabizadeh@gmail.com>
2025-05-28 23:20:33 +0330

    railwaybuilds

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,18 +1,21 @@
 'use client';
 
 import { ReactNode } from 'react';
+import { SessionProvider } from 'next-auth/react';
 import { UserProvider } from './context/UserProvider';
 import { ErrorProvider } from './context/ErrorProvider';
 import { TerminalProvider } from './context/TerminalProvider';
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <UserProvider>
-      <ErrorProvider>
-        <TerminalProvider>
-          {children}
-        </TerminalProvider>
-      </ErrorProvider>
-    </UserProvider>
+    <SessionProvider>
+      <UserProvider>
+        <ErrorProvider>
+          <TerminalProvider>
+            {children}
+          </TerminalProvider>
+        </ErrorProvider>
+      </UserProvider>
+    </SessionProvider>
   );
 } 
